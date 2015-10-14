c674f31ed1508e1ab5c51f6c9aaac8a513187a9d
jinlong <jinlongwang89@gmail.com>
2015-10-14 10:06:37 +0800

    修改next_url

--- handlers/SignInHandler.py
+++ handlers/SignInHandler.py
@@ -11,16 +11,16 @@ class SignInHandler(BaseHandler):
     def get(self):
         if self.current_user:
             return self.redirect("/index")
-        next = self.get_argument("next", "")
-        self.render("login.html", next=next)
+        next_url = self.get_argument("next", "")
+        self.render("login.html", next_url=next_url)
 
     def post(self):
         user_name = self.get_argument("email", None)
         password = self.get_argument("password", None)
-        next_url = self.get_argument("next", None)
+        next_url = self.get_argument("next", "/")
         user = self.datastore.check_user(user_name, password)
         if not user:
-            return self.render("login.html", error=u'用户名或密码错误')
+            return self.render("login.html", error=u'用户名或密码错误', next_url=next_url)
         self.set_current_user(user.user_name, user.id, 1)
         if next_url:
             return self.redirect(next_url)
